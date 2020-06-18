4dc6115d28cb13c675ac31d90edaed256510ff9a
youssefMes <youssef-mestiri@outlook.fr>
2020-06-18 21:01:53 +0200

    setup mongodb && elasticsearch docker containers

--- cranach-timeline-code/server/index.js
+++ cranach-timeline-code/server/index.js
@@ -1,5 +1,23 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+// Connect to MongoDB
+mongoose
+    .connect(
+        'mongodb://cranach_mongodb:27017/db_cranach',
+        { useNewUrlParser: true, useUnifiedTopology: true }
+    )
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
+
+
+
+app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(9000, () => console.log(`server app listening at http://localhost:9000`));
