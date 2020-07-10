9128611f375cc039d95f1519cfbfb86ffd065710
Rami Cheikh Rouhou <ramicheikhrouhou670@gmail.com>
2020-07-10 14:07:16 +0200

    Update timeline.component.jsx

--- cranach-timeline-code/client/src/components/timeline/timeline.component.jsx
+++ cranach-timeline-code/client/src/components/timeline/timeline.component.jsx
@@ -5,26 +5,26 @@ import './timeline.style.css';
 const VALUES = ['2020-06-27', '2020-07-01', '2020-08-01', '2020-09-01', '2020-10-01', '2020-11-01', '2020-12-01', '2020-08-01', '2021-01-01', '2021-01-01'];
 
 export default class Timeline extends React.Component {
-  state = { value: 0, previous: 0 };
+    state = { value: 0, previous: 0 };
 
-  render() {
-    return (
-      <div>
-        {/* Bounding box for the Timeline */}
-        <div style={{ width: '100%', height: '100px', margin: '0 auto' }}>
-          <HorizontalTimeline
-            index={this.state.value}
-            indexClick={(index) => {
-              this.setState({ value: index, previous: this.state.value });
-            }}
-            values={ VALUES } />
+    render() {
+        return (
+        <div>
+            {/* Bounding box for the Timeline */}
+            <div style={{ width: '100%', height: '100px', margin: '0 auto' }}>
+            <HorizontalTimeline
+                index={this.state.value}
+                indexClick={(index) => {
+                this.setState({ value: index, previous: this.state.value });
+                }}
+                values={ VALUES } />
+            </div>
+            <div className='timeline-image'>
+            {/* any arbitrary component can go here */}
+            <img src="https://source.unsplash.com/random" alt="Logo" />;
+            {this.state.value}
+            </div>
         </div>
-        <div className='timeline-image'>
-          {/* any arbitrary component can go here */}
-          <img src="https://source.unsplash.com/random" alt="Logo" />;
-          {this.state.value}
-        </div>
-      </div>
-    );
-  }
-}
+        );
+    }
+    }
