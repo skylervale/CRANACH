b5fec5a6a065d9a6f6d31f34eb495d7f79e5fc84
youssefMes <youssef-mestiri@outlook.fr>
2020-10-27 16:04:54 +0100

    adjust filters add location filter

--- cranach-timeline-code/server/controllers/graphic.controller.js
+++ cranach-timeline-code/server/controllers/graphic.controller.js
@@ -93,6 +93,7 @@ const FullTextSearch = async function (req, res) {
     const medium = req.query.medium
     const repositories = req.query.repositories ? req.query.repositories : []
     const owners = req.query.owners ? req.query.owners : []
+    const locations = req.query.locations ? req.query.locations : []
     const body = {
         query: {
             bool: {
@@ -203,18 +204,29 @@ const FullTextSearch = async function (req, res) {
         }
         body.query.bool.filter.push(filterItem)
     }
-
+    if (locations.length > 0){
+        console.log("locations", locations)
+        const filterItem = {
+            "nested": {
+                "path": "locations",
+                "query": {
+                    "terms": {"locations.term": locations}
+                }
+            }
+        }
+        body.query.bool.filter.push(filterItem)
+    }
     if (repositories.length > 0){
         console.log("repositories", repositories)
 
         const filterItem = {
-            "terms": {"repository": repositories}
+            "terms": {"repository.raw": repositories}
         }
         body.query.bool.filter.push(filterItem)
     }
     if (owners.length > 0){
         const filterItem = {
-            "terms": {"owner": owners}
+            "terms": {"owner.raw": owners}
         }
         body.query.bool.filter.push(filterItem)
     }
